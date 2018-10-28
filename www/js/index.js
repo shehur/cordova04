a542268d392ebffc4aaad2b48cc850f3331dfbda
shehur <shehur5727@gmail.com>
2018-10-27 22:25:44 -0300

    update 01

---
+++ www/js/index.js
@@ -0,0 +1,17 @@
+﻿var app = {
+    initialize: function() {
+        this.bindEvents();
+    },
+
+    bindEvents: function() {
+        document.addEventListener('deviceready', this.onDeviceReady, false);
+    },
+
+    onDeviceReady: function() {
+        app.receivedEvent('deviceready');
+    },
+
+    receivedEvent: function(id) {
+        
+    }
+};
